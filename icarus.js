d76270d1608548c818f1947a48597f9a90a575b8
Dewitt Buckingham <dj729@comcast.net>
2018-02-08 16:55:29 -0800

    Link Routes to Server

--- icarus.js
+++ icarus.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const app = express();
+const routes = require('./app/routes');
 const config = require('./config').SERVER;
 
 app.use(morgan('dev'));
@@ -12,13 +13,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(methodOverride());
 
-mongoose.connect(config.db);
+console.log('Icarus is taking flight...\n')
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
 
+mongoose.connect(config.db);
 mongoose.connection.on('connected', () => {
-  console.log(`Mongoose default connection open to ${config.db}`);
+  console.log(`DB_URL: ${config.db}`);
 });
 
+// User Routes
+app.use('/user', routes.USER);
+
+// Catch-All Route for Errors
+app.get('*', (req, res) => {
+  res.send('Does not compute.');
+});
+
+// Launch Server
 app.listen(config.port, err => {
   if (err) throw err;
-  console.log(`App listening on port ${config.port}`);
+  console.log(`PORT: ${config.port}`);
 });
