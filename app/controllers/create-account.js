6d842684f2c0ebafef74440ccf91126d4881b418
Dewitt Buckingham <dj729@comcast.net>
2018-02-09 10:33:21 -0800

    Add Leasot for TODO Tracking

--- app/controllers/create-account.js
+++ app/controllers/create-account.js
@@ -1,16 +1,22 @@
 const bcrypt = require('bcrypt');
 const user = require('../models').USER;
 
+/* TODO: Looks ugly. Refactor.*/
+
 function createAccount(req, res, next) {
   const { email, firstName, lastName, organization, phone } = req.body;
   console.log(req);
   const saltRounds = 10;
 
+  // Generate Salt
   bcrypt.genSalt(saltRounds, (err, salt) => {
     if (err) return next(err);
+
+    // Generate Hash
     bcrypt.hash(req.body.password, salt, (err, hash) => {
       if (err) return next(err);
 
+      // Build New User Data object
       const userData = {
         email,
         firstName,
@@ -20,6 +26,7 @@ function createAccount(req, res, next) {
         passwordHash: hash,
       };
 
+      // Submit new User object to database. Then log in.
       user.create(userData, (err, user) => {
         if (err) throw err;
         req.login(user, err => {
