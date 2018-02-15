7d107611006b7aea4d035be4cd34fc458bc5b638
Dewitt Buckingham <dj729@comcast.net>
2018-02-14 19:26:06 -0800

    Server model

---
+++ app/models/server.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const serverSchema = new Schema({
+  LINODEID: { type: Number, unique: true, index: true, required: true },
+  TOTALXFER: { type: Number, required: true },
+  TOTALRAM: { type: Number, required: true },
+  TOTALHD: { type: Number, required: true },
+  LABEL: { type: String, required: true, default: 'No Label' },
+  STATUS: { type: Number, required: true },
+  DATACENTERID: { type: Number, required: true },
+  CREATE_DT: { type: Date, required: true },
+  PLANID: { type: Number, required: true },
+  DISTRIBUTIONVENDOR: {
+    type: String,
+    required: true,
+    default: 'NO Distribution/Vendor',
+  },
+});
+
+module.exports = mongoose.model('server', serverSchema);
