a5f17d2240c7aaf8a0019b936730e99d0ea0bc9b
Dewitt Buckingham <dj729@comcast.net>
2018-02-23 22:27:35 -0800

    Separate Hosting/Support Plans
    Now users can build their own discrete packages.

--- app/assets/signup.js
+++ app/assets/signup.js
@@ -1,17 +1,35 @@
 $(function() {
   setPlan();
-  $('#hosting-plan').change(setPlan);
+  $('#hardware').change(setPlan);
+  $('#support-plan').change(setPlan);
 });
 
 function setPlan() {
-  if ($('#hosting-plan').val() === 'hands-off') {
-    $('#plan_title').html('1 year of Hands-Off Hosting');
-    $('#plan_total').html('$300');
-  } else if ($('#hosting-plan').val() === 'maintain') {
-    $('#plan_title').html('1 year of Maintain Hosting');
-    $('#plan_total').html('$425');
-  } else if ($('#hosting-plan').val() === 'hands-on') {
-    $('#plan_title').html('1 year of Hands-On Hosting');
-    $('#plan_total').html('$550');
+  // Set Hardware
+  if ($('#hardware').val() === 'starter') {
+    $('#hardware_title').html('1 year of Starter Hosting');
+    $('#hardware_price').html('$195');
+  } else if ($('#hardware').val() === 'business') {
+    $('#hardware_title').html('1 year of Business Hosting');
+    $('#hardware_price').html('$295');
+  } else if ($('#hardware').val() === 'developer') {
+    $('#hardware_title').html('1 year of Developer Hosting');
+    $('#hardware_price').html('$495');
+  } else {
+    console.log('Not Selected');
+  }
+
+  // Set Support Plan
+  if ($('#support-plan').val() === 'hands-off') {
+    $('#support_title').html('1 year of Hands-Off Support');
+    $('#support_price').html('Free');
+  } else if ($('#support-plan').val() === 'maintain') {
+    $('#support_title').html('1 year of Maintain Support');
+    $('#support_price').html('$125');
+  } else if ($('#support-plan').val() === 'hands-on') {
+    $('#support_title').html('1 year of Hands-On Support');
+    $('#support_price').html('$375');
+  } else {
+    console.log('Not Selected');
   }
 }
