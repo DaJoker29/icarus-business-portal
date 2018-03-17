2ece164ac78cfbe8a7fe2bca05e5d1b4468f4f07
Dewitt Buckingham <dj729@comcast.net>
2018-03-16 23:06:55 -0700

    Users can View Past Transactions
    Closes #58

--- app/routes/payment.js
+++ app/routes/payment.js
@@ -6,4 +6,6 @@ router.get('/payment', Auth.AUTHENTICATED, Payment.RENDER_PAYMENT_FORM);
 
 router.post('/payment/customer', Auth.AUTHENTICATED, Payment.CREATE_STRIPE_ID);
 
+router.get('/payments', Auth.AUTHENTICATED, Payment.RENDER_PAST_PAYMENTS);
+
 module.exports = router;
